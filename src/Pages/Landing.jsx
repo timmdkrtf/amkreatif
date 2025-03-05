5b336f9080632cfcaa9fe0f98ae025f71b071fa2
timmdkrtf <contact@mdkrtf.co.id>
2025-03-05 10:36:17 +0700

    add about us and responsive about us

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar"
 import Cover from "../components/Cover"
 import AboutPT from "../components/AboutPT"
+import AboutUs from "../components/AboutUs"
 import Brand from "../components/Brand"
 import Footer from "../components/Footer"
 
@@ -10,6 +11,7 @@ export default function Landing(){
             <Navbar />
             <Cover />
             <AboutPT />
+            <AboutUs />
             <Brand />
             <Footer />
         </div>
--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,10 +1,39 @@
-
+import AdiPhoto from "../assets/image/photo/photo-ustad.png"
+import HafizPhoto from "../assets/image/photo/Photo-hafiz.png"
 
 export default function AboutUs(){
     return(
         <div className="about-us">
-            <div className="">
-
+            <div className="row">
+                <div className="content-left">
+                    <div className="cover-image">
+                        <img src={AdiPhoto} alt="" />
+                    </div>
+                    <div className="title">
+                        <h4>Irianto Setiadi</h4>
+                        <h5>Dewan Komisaris</h5>
+                    </div>
+                </div>
+                <div className="content-center">
+                    <div className="title">
+                        <h2>
+                            Dewan Komisaris
+                            <br/>
+                            <span>&</span>
+                            <br/>
+                            Direksi Utama
+                        </h2>
+                    </div>
+                </div>
+                <div className="content-right">
+                    <div className="cover-image">
+                        <img src={HafizPhoto} alt="" />
+                    </div>
+                    <div className="title">
+                        <h4>Muhammad Hafiz Mauludi</h4>
+                        <h5>Direksi Utama</h5>
+                    </div>
+                </div>
             </div>
         </div>
     );
