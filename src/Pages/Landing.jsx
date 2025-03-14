382cc6de2ee23f6abad0f4ac050a4d27811c1c19
timmdkrtf <contact@mdkrtf.co.id>
2025-03-14 21:16:48 +0700

    under maintenance

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,19 +1,28 @@
-import Navbar from "../components/Navbar"
-import Cover from "../components/Cover"
-import AboutPT from "../components/AboutPT"
-import AboutUs from "../components/AboutUs"
-import Brand from "../components/Brand"
-import Footer from "../components/Footer"
+// import Navbar from "../components/Navbar"
+// import Cover from "../components/Cover"
+// import AboutPT from "../components/AboutPT"
+// import AboutUs from "../components/AboutUs"
+// import Brand from "../components/Brand"
+// import Footer from "../components/Footer"
+
+import AMKLogo from "../assets/image/logo/AMK (P).png"
 
 export default function Landing(){
     return(
-        <div className="Landing">
-            <Navbar />
+        <>
+            {/* <Navbar />
             <Cover />
             <AboutPT />
             <AboutUs />
             <Brand />
-            <Footer />
-        </div>
+            <Footer /> */}
+
+            <div className="landing">
+                <div className="content">
+                    <img src={AMKLogo} alt="" />
+                    <h3>Under Maintenance</h3>
+                </div>
+            </div>
+        </>
     );
 }
