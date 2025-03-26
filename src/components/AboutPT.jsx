72879f7299bd720982c59a27f4fcc33185bc3f03
timmdkrtf <contact@mdkrtf.co.id>
2025-03-26 09:24:24 +0700

    revisi text about us

--- src/components/AboutPT.jsx
+++ src/components/AboutPT.jsx
@@ -9,7 +9,7 @@ export default function AboutPT(){
                     <div className="text">
                         <p>PT Abdi Media Kreatif didirikan pada tahun 2023. Kami adalah perusahaan yang bergerak di bidang media kreatif yang merupakan anak perusahaan dari Thamrin Meyer Corporation.</p>
                         <p>PT Abdi Media Kreatif berkomitmen untuk menembus batas-batas kreativitas dan menciptakan karya-karya inspiratif.</p>
-                        <p>Melalui brand mdkrtf, kami memiliki dedikasi dan keunggulan dalam industri media kreatif, kami terus berupaya untuk menghadirkan solusi yang relevan dan berdampak, memadukan seni, teknologi, dan strategi untuk menciptakan pengalaman yang bernilai bagi audiens dan klien kami.</p>
+                        <p>Melalui <span className="fst-italic">brand</span> mdkrtf, kami memiliki dedikasi dan keunggulan dalam industri media kreatif, kami terus berupaya untuk menghadirkan solusi yang relevan dan berdampak, memadukan seni, teknologi, dan strategi untuk menciptakan pengalaman yang bernilai bagi audiens dan klien kami.</p>
                     </div>
                 </div>
             </div>
