973ecbb34b7b49b89d4554c90341b6a40b21884c
timmdkrtf <contact@mdkrtf.co.id>
2025-04-15 09:10:34 +0700

    publish

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,28 +1,19 @@
-// import Navbar from "../components/Navbar"
-// import Cover from "../components/Cover"
-// import AboutPT from "../components/AboutPT"
-// import AboutUs from "../components/AboutUs"
-// import Brand from "../components/Brand"
-// import Footer from "../components/Footer"
-
-import AMKLogo from "../assets/image/logo/AMK (P).png"
+import Navbar from "../components/Navbar"
+import Cover from "../components/Cover"
+import AboutPT from "../components/AboutPT"
+import AboutUs from "../components/AboutUs"
+import Brand from "../components/Brand"
+import Footer from "../components/Footer"
 
 export default function Landing(){
     return(
         <>
-            {/* <Navbar />
+            <Navbar />
             <Cover />
             <AboutPT />
             <AboutUs />
             <Brand />
-            <Footer /> */}
-
-            <div className="landing">
-                <div className="content">
-                    <img src={AMKLogo} alt="" />
-                    <h3>Under Maintenance</h3>
-                </div>
-            </div>
+            <Footer />
         </>
     );
 }
